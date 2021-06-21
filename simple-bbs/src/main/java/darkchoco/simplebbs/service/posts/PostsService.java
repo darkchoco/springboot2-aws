a8a988b13343133aec9d58168b0691d5c032316f
darkchoco <darkchoco@cloudscape.kr>
2021-06-21 16:21:44 +0200

    springboot2-aws: [ch04] completed up to findAllDec().

--- simple-bbs/src/main/java/darkchoco/simplebbs/service/posts/PostsService.java
+++ simple-bbs/src/main/java/darkchoco/simplebbs/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package darkchoco.simplebbs.service.posts;
 
 import darkchoco.simplebbs.domain.posts.Posts;
 import darkchoco.simplebbs.domain.posts.PostsRepository;
+import darkchoco.simplebbs.web.dto.PostsListResponseDto;
 import darkchoco.simplebbs.web.dto.PostsResponseDto;
 import darkchoco.simplebbs.web.dto.PostsSaveRequestDto;
 import darkchoco.simplebbs.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor  // final이 선언된 모든 필드를 인자값으로 하는 생성자를 생성. 덕분에 아래 @Autowired 가 없다.
 @Service
@@ -35,5 +38,10 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDec() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
