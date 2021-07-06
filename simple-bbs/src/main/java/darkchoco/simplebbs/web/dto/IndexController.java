99ffd7448504316d4e5cc5c330c1df6b3d8e78ee
darkchoco <darkchoco@cloudscape.kr>
2021-07-06 14:53:25 +0200

    springboot2-aws: [ch05] Google login integration.

--- simple-bbs/src/main/java/darkchoco/simplebbs/web/dto/IndexController.java
+++ simple-bbs/src/main/java/darkchoco/simplebbs/web/dto/IndexController.java
@@ -1,20 +1,27 @@
 package darkchoco.simplebbs.web.dto;
 
+import darkchoco.simplebbs.config.auth.dto.SessionUser;
 import darkchoco.simplebbs.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDec());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null)
+            model.addAttribute("userName", user.getName());
         return "index";
     }
 
